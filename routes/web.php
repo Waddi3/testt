ba5fe4aa10aecd37b7946d2c80364f545462d1a6
waddi3 <waddiaalmohammad1@gmail.com>
2024-08-30 22:08:37 +0330

    new cimmit

--- routes/web.php
+++ routes/web.php
@@ -99,5 +99,5 @@ Route::prefix('/fun')->name('fun.')->group(function() use($posts){
         return response()->download(public_path('/Screenshot from 2024-08-25 13-59-44.png'), 'screenshot');
     })->name('download');
 });
-///gggggggg
+
 
